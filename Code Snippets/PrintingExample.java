a67a0ed2a7be8e1baede8592ede337b407bee7af
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-03 14:48:04 +0600

    added some formatted printing examples

--- Code Snippets/PrintingExample.java
+++ Code Snippets/PrintingExample.java
@@ -15,6 +15,35 @@ public class PrintingExample {
         }else{
             System.out.println(first_name +" "+ middle_name +" "+ last_name);
         }
+
+        //formatted strings;
+        int num1 = 24;
+        int num2 = -23;
+        int big_num = 234567976;
+        System.out.printf("%2$,3.2f %1$s\n", "meters", 1260.5052);
+        
+        // Default
+        System.out.printf("%f%n", 123456.78);
+
+        // Two decimal digits
+        System.out.printf("%.2f%n", 123456.78);
+
+        // No decimal digits
+        System.out.printf("%.0f%n", 123456.78);
+
+        // No decimal digits but keep the decimal point
+        System.out.printf("%#.0f%n", 123456.78);
+
+        // Group digits
+        System.out.printf("%,.2f%n", 123456.78);
+
+        // Scientific notation with two digits of precision
+        System.out.printf("%.2e\n", 123456.78);
+
+        //finally
+        float f1 = 212224.3436573f;
+        int num3 = 234546;
+        System.out.printf("This %1$+,.2f and %2$+,d is showing with printf\n", f1, num3);
         scanner.close();
     }
 }
