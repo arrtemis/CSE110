b247a01b7e6e581af0b69110a473785af24b5828
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-06 13:30:50 +0600

    lab shit

---
+++ Code Snippets/last_two_digit.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class last_two_digit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 1234;
+        System.out.println(num1 % 100);
+        scanner.close();
+    }
+}
