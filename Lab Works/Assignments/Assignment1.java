6c836c6122c25203a04958bc929d902cfd01973b
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-06 10:12:04 +0600

    task 10

--- Lab Works/Assignments/Assignment1.java
+++ Lab Works/Assignments/Assignment1.java
@@ -1,1 +1,16 @@
+public class lab_assignments{
+  public static void main(String[] args){
+    int hungry;
+    int 2AB;
+    int 312.2;
+    int MOBILE;
+    int "Ans";
+    int $30;
+    int Yes/No;
+    int student-id;
+    int A+3;
+    int 'X';
+    int return;
+  }
+}
 
