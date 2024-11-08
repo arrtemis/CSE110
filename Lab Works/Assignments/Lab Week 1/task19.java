2307f22cb4bc1d439c0881e7dce2fb2c5ca304a3
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-08 09:36:04 +0600

    finished task 19 and 20

---
+++ Lab Works/Assignments/Lab Week 1/task19.java
@@ -0,0 +1,8 @@
+public class task19 {
+    public static void main(String[] args) {
+        double a = 8, b = 3, c = 5; // c = sqrt((8/2)^2 + 3^2)
+        double area =  a * b + a * c;
+        double circumference = c * 6;
+        System.out.println("Area = " + area + "\nCircumference = " + circumference);
+    }
+}
