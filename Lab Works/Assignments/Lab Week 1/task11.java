604f3651187490fdfd75c66f5ded33f2e5cdc215
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-11 20:39:40 +0600

    finalize task11

--- Lab Works/Assignments/Lab Week 1/task11.java
+++ Lab Works/Assignments/Lab Week 1/task11.java
@@ -14,27 +14,27 @@ public class task11 {
 
         //2.4
         double num3 = 23, num4 = 24, sum, product, quotient;
-        sum = num3 + num4;
-        product = num3 * num4;
-        quotient = num3 / num4;
+        sum = num3 + num4; //23.0 + 24.0 = 47.0
+        product = num3 * num4; //23.0 * 24.0 = 552.0
+        quotient = num3 / num4; //23.0 / 24.0 = 0.958 (truncated)
         System.out.println("Sum = " + sum + "\nProduct = " + product + "\nQuotient = " + quotient);
 
         //2.5
-        int num5 = 23;
-        double num6 = 24;
-        sum = num5 + num6;
-        product = num5 * num6;
-        quotient = num5 / num6;
+        int num5 = 90;
+        double num6 = 10;
+        sum = num5 + num6; //90.0(int -> double) + 10.0 = 100.0
+        product = num5 * num6; //90.0(int -> double) * 10 = 900.0
+        quotient = num5 / num6; //90.0(int -> double) / 10 = 9.0
         System.out.println("Sum = " + sum + "\nProduct = " + product + "\nQuotient = " + quotient);
 
         //2.6
-        String str1 = "23", str2 = "24huh";
-        String sum_str = str1 + str2;
-        String sum_diff =  num1 + str1;
-        //int sum_diff_int = num1 + str1; 
-        //string cannot be converted to int, hence invalid operation
-
+        String str1 = "cse110", str2 = " is fun";
+        String addition_str = str1 + str2; //"cse110" + "is fun" = "cse110 is fun"
         System.out.println("Addition of strings = " + sum_str + "\nAddition of an int and string = invalid");
+        //the addition operator joins two string together.
+        
+        //if the first variable was an int and the second variable was a string and vice versa then the addition would still be valid, e.g:
+        System.out.println(num1 + str1);
 
     }
 }
