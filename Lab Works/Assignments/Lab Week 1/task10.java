d8e03a6026366c991a2fb9c582fd2baeb8a3398d
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-11 20:39:59 +0600

    edit task10

--- Lab Works/Assignments/Lab Week 1/task10.java
+++ Lab Works/Assignments/Lab Week 1/task10.java
@@ -4,12 +4,12 @@ public class task10{
     int 2AB; //invalid, variable names cannot start with a number
     int 312.2; //invalid, variables names cannot be a number
     int MOBILE; //valid
-    int "Ans"; //invalid, variable names cannot have double quotation  marks
+    int "Ans"; //invalid, variable names cannot have special characters
     int $30; //valid
-    int Yes/No; //invalid
+    int Yes/No; //invalid, variable names cannot have special characters
     int student-id; //invalid, variable names cannot contain operators
     int A+3; //invalid, variable names cannot contain operators
-    int 'X'; //invalid, variable names cannot contain single quotation marks
+    int 'X'; //invalid, variable names cannot have special characters
     int return; //invalid, reserved keyword in java
   }
 }
