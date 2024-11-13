722359ab4f84a8da375b42159fdde0521cebe335
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-13 11:43:29 +0600

    finished task2

--- Lab Works/Assignments/Lab Week 2/task2.java
+++ Lab Works/Assignments/Lab Week 2/task2.java
@@ -7,15 +7,27 @@ public class task2 {
         System.out.println("Please enter your marks: ");
         int marks = sc.nextInt();
         if(marks <= 100 && marks >= 0){
-            if(marks >= 90 && marks <= 100){
+            if(marks >= 90){
                 grade = "A";
             }
-            if(marks <= 89 && marks >= 85){
+            if(marks >= 85 && marks <= 89){
                 grade = "A-";
+            }
+            if(marks >= 70 && marks <= 84){
+                grade = "B";
+            }
+            if(marks >= 57 && marks <= 69){
+                grade = "C";
+            }
+            if(marks >= 50 && marks <= 56){
+                grade = "D";
+            }else{
+                grade = "F";
             }
         }else{
             System.out.println(marks + " is not a valid mark");
         }
         System.out.println("Your grade is: " + grade);
+        sc.close();
     }
 }
