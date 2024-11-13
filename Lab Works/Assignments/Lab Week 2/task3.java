025d262d600ae021ec29c5689388c1a0c0768bcd
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-13 12:06:06 +0600

    finished task 3

---
+++ Lab Works/Assignments/Lab Week 2/task3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[] args) {
+        /*if(divisible by both 5 & 7){
+        *}else{
+        * no}
+        */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please give a number: ");
+        int number = sc.nextInt();
+
+        int divisible = 0;
+        if(number % 5 == 0){
+            divisible++;
+        }
+        if(number % 7 == 0){
+            divisible++;
+        }
+
+        if(divisible == 1){
+            if(number % 5 == 0){
+                System.out.println("Invalid: Divisible by 5 only");
+            }else if(number % 7 == 0){
+                System.out.println("Invalid: Divisible by 7 only");
+            }
+        }else if(divisible == 2){
+            System.out.println("Divisible by both");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
