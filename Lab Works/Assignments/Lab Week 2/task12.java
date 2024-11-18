dc6045e12c119bde7fb46fe27e70e4e4f96ddfd2
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-18 06:29:05 +0600

    task 12

---
+++ Lab Works/Assignments/Lab Week 2/task12.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class task12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, num3;
+    }
+}
