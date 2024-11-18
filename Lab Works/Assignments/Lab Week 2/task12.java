1eae5a52b849f2527b56ce5eca95104fb8e3df9b
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-18 06:32:35 +0600

    pain

--- Lab Works/Assignments/Lab Week 2/task12.java
+++ Lab Works/Assignments/Lab Week 2/task12.java
@@ -4,5 +4,15 @@ public class task12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1, num2, num3;
+        System.out.println("Please enter 3 numbers: ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        num3 = sc.nextInt();
+
+        if(num1 == num2 && num2 == num3){
+            System.out.println("All numbers are equal");
+        }else if(num1 != num2{
+
+        }
     }
 }
