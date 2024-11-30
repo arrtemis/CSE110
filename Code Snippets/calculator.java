96bea198d11a2aa7bf70ce17ba2624843f00b8e2
Jayed Yeameen <yeameenjayed@gmail.com>
2024-11-30 11:54:28 +0600

    big updates on calculator

--- Code Snippets/calculator.java
+++ Code Snippets/calculator.java
@@ -3,63 +3,74 @@ import java.util.Scanner;
 
 public class calculator{
     public static void main(String args[]){
+        System.out.println("Welcome user! The calculator is currently configured to display 8 significant figures and the angle unit is in degrees. Happy calculating!");
+        //the keypad
         Scanner keypad = new Scanner(System.in);
 
-        int a, b, c;
-        double result;
-        String prompt;
+        //the variables
+        double factorial = 1, angle_rad, angle_degree, sum = 0.0, result = 0.0, a = 0.0, b = 0.0;
+        String prompt, operator = null;
+        System.out.println("more modes are WIP if uni doesn't kill me, but for now, algebra or trigonometry?: ");
+        prompt = keypad.nextLine();
 
-        System.out.println("please enter the numbers: ");
-        a = keypad.nextInt();
-        b = keypad.nextInt();
+        //the place where the magic happens (asking for user input and deciding where to go from there)
+        if(prompt.equalsIgnoreCase("algebra")){ //algebra mode
+            System.out.println("Please enter the numbers: ");
+            a = keypad.nextDouble();
+            b = keypad.nextDouble();
 
-        keypad.nextLine();
-        System.out.println("now enter the operator (valid options: +, -, *, /, ^, %, sin, cos, tan): ");
-        String operator = keypad.nextLine();
+            keypad.nextLine();
+            System.out.println("Now enter the operators(+ - * / ^ % !): ");
+            operator = keypad.nextLine();
 
+        }else if(prompt.equalsIgnoreCase("trigonometry")){ //trig mode
+            System.out.print("sin cos tan\n=>");
+            operator = keypad.nextLine();
+        }else{
+            operator = "invalid";
+        }
+
+        //the calculator
         switch (operator) {
             case "+":
                 result = a + b;
-                System.out.println(result);
                 break;
             case "-":
                 result = a - b;
-                System.out.println(result);
                 break;
             case "*":
                 result = a * b;
-                System.out.println(result);
                 break;
             case "/":
                 result = a / b;
-                System.out.println(result);
                 break;
             case "^":
                 result = Math.pow(a, b);
-                System.out.println(result);
                 break;
             case "%":
                 result = a % b;
-                System.out.println("The remainder when " + a + " is divided by " + b + " is = " + result);
+                System.out.println("The remainder is " + (float)result + " when " + a + " is divided by " + b);
                 break;
             case "sin":
-                System.out.println("please enter the third side: ");
-                c = keypad.nextInt();
-                if(a > b && a > c){
-                    System.out.println("Assuming " + b + " to be the length of opposite side\nDo you want to proceed?(y/n): ");
-                    prompt = keypad.nextLine();
-                    if(prompt == "y"){
-                        result = b/a;
-                        System.out.println("sin = " + result);
-                    }else if(prompt == "n"){
-                        System.out.println("Okay, swapping the sides.");
-                        result = c/a;
-                        System.out.println("sin = " + result);
+                System.out.println("angle(in degrees): ");
+                angle_degree = keypad.nextDouble();
+
+                if(angle_degree > 360 || angle_degree < -360){
+                    angle_degree %= 360;
+                }
+
+                angle_rad = Math.toRadians(angle_degree);
+                for(int n = 0; n <= 100; n++){    
+                    factorial = 1;
+                    for(int j = 1; j <= 2*n + 1; j++){
+                        factorial *= j;
+                    }
+                    if(n % 2 == 0){
+                        result += (Math.pow(angle_rad, 2*n + 1) / factorial);
+                    }else{
+                        result -= (Math.pow(angle_rad, 2*n + 1) / factorial);
                     }
                 }
-                //to-do: add this
-                if(b > a && b > c){}
-                if(c > a && c > b){}
                 break;
             case "cos":
                 break;
@@ -69,6 +80,8 @@ public class calculator{
                 System.out.println("Sorry the calculator has not yet been programmed to perform this operation :(");
                 break;
         }
+
+        System.out.println("\n= " + (float)result);
 
         keypad.close();
     }
