b5a59e94b8b89c4272d30fe07760f87fbf987c10
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-04 11:29:21 +0600

    task 3 done

--- Lab Works/Assignments/Lab Week 4/task3.java
+++ Lab Works/Assignments/Lab Week 4/task3.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class task3 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n, num, pos = 0, neg = 0;
+
+        System.out.print("Enter an integer: ");
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.print("Enter number " + i + ": ");
+            num = sc.nextInt();
+            if(num >= 0){
+                pos++;
+            }else{
+                neg++;
+            }
+        }
+        System.out.println(pos + " Non-negative numbers");
+        System.out.println(neg + " Negative numbers");
+        sc.close();
     }
 }
 
