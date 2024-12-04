7d6bfcefd0a0ae62db5a68fcb2d8ec43ef897f44
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-04 09:39:44 +0600

    Update task1.java

--- Lab Works/Assignments/Lab Week 4/task1.java
+++ Lab Works/Assignments/Lab Week 4/task1.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class task1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int inputs, max = 0, min = Integer.MAX_VALUE, average = 0, sum = 0, num, count = 0;
+        inputs = sc.nextInt();
+
+        while(inputs != 0){
+            num = sc.nextInt();
+
+            if(num > 0 && num % 2 == 0){
+                if(num > max){ max = num; }
+                if(num < min){ min = num; }
+                count++;
+                sum += num;
+            }
+
+            if(count == 0){
+                average = 0; 
+            }else{
+                average = sum / count;
+            }
+
+            inputs--;
+        }
+        System.out.println("Max: " + max + "\nMin: " + min + "\nAverage: " + average);
+        sc.close();
+    }
+}
