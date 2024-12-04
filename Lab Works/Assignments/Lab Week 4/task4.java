b24800fc65adda7c6186c4a658858d67b9cbc031
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-04 19:50:33 +0600

    task 4 done

--- Lab Works/Assignments/Lab Week 4/task4.java
+++ Lab Works/Assignments/Lab Week 4/task4.java
@@ -1,6 +1,25 @@
-public class task3 {
+import java.util.Scanner;
+
+public class task4 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n, num = 2, divisors, prime_count = 0;
+        n = sc.nextInt();
+
+        while(prime_count < n){
+            divisors = 0;
+            for(int j = 1; j <= num; j++){
+                if(num % j == 0){
+                    divisors++;
+                }
+            }
+            if(divisors == 2){
+                prime_count++;
+                System.out.println(num);
+            }
+            num++;
+        }
+        sc.close();
     }
 }
 
