c632fcd9fa2d80e9a14f6c754ee4c4aaca979820
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-04 10:15:10 +0600

    Create task8.java

---
+++ Lab Works/Assignments/Lab Week 4/task8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows, columns;
+        columns = sc.nextInt();
+        rows = sc.nextInt();
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= columns; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
