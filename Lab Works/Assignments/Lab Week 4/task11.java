e8ef1d59688e6c4ba65eef052b0fdedef15224cd
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-05 08:49:45 +0600

    finished task 11 fr

--- Lab Works/Assignments/Lab Week 4/task11.java
+++ Lab Works/Assignments/Lab Week 4/task11.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class task11 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int start, end, digit = 0, result = 0;
+
+        System.out.print("Start: ");
+        start = sc.nextInt();
+        System.out.print("End: ");
+        end = sc.nextInt();
+
+        System.out.println("Armstrong numbers: ");
+        for(int i = start; i <= end; i++){
+            int j = i;
+            while (j != 0) {
+                digit = j % 10;
+                result += (digit * digit * digit);
+                j /= 10;
+            }if (result == i) {
+                System.out.println(i);   
+            }
+            start++;
+            result = 0;
+        }
+        sc.close();
     }
 }
