5907c06cd3370434abc9c95b35018495b552a5e1
Jayed Yeameen <yeameenjayed@gmail.com>
2024-12-05 16:02:02 +0600

    tracing problems ass 4 and mid q1 updated

--- Lab Works/Class Work/mid_q1.java
+++ Lab Works/Class Work/mid_q1.java
@@ -1,33 +1,45 @@
 public class mid_q1 {
     public static void main(String[] args) {
-        int n = 15498934, m = n, power = 1, digits, ao = n, ao_digits, sum_digits, count = 0;
+        int n = 15489723, m = n, digits, sum_digits, rev = 0;
 
         while(n / 10 != 0){
             digits = n % 100;
-            sum_digits = digits / 10 + digits % 10; 
-            if(sum_digits >= 10){
-                sum_digits %= 10;
-            }
+            sum_digits = (digits / 10 + digits % 10) % 10; 
             n = (n/100) * 10 + sum_digits;
         }
         System.out.println("checksum: " + n);
 
+        if(n % 2 == 0){
+            System.out.println("Checksum is even");
+        }else{
+            System.out.println("Checksum is odd");
+        }
+
         while(m != 0){
-            count++;
-            m /= 10;
+            digits = m % 10;
+            rev = rev*10 + digits;
+            m /=10;
         }
-        for(int i = 2; i <= count; i++){
-            power *= 10;
-        }
-        while(ao != 0){
-            ao_digits = ao / power;
-            if(ao_digits % 2 == 0 && n % 2 == 0){
-                System.out.print(ao_digits + " ");
-            }else if(ao_digits % 2 == 1 && n % 2 == 1){
-                System.out.print(ao_digits + " ");
+        digits = 0;
+
+        if(n % 2 == 0){
+            System.out.print("Even digits of the number: ");
+            while(rev != 0){
+                digits = rev % 10;
+                if(digits % 2 == 0){
+                    System.out.print(digits + " ");
+                }
+                rev /= 10;
             }
-            ao %= power;
-            power /= 10;
+        }else{
+            System.out.print("Odd digits of the number: ");
+            while(rev != 0){
+                digits = rev % 10;
+                if(digits % 2 == 1){
+                    System.out.print(digits + " ");
+                }
+                rev /= 10;
+            }
         }
     }
 }
